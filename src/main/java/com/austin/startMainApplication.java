c69dc002c8250a68988475994fadb0cc36e43fb9
austin-lee <seokjun88@gmail.com>
2019-07-20 14:08:04 +0900

    first commit

---
+++ src/main/java/com/austin/startMainApplication.java
@@ -0,0 +1,12 @@
+package com.austin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class startMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(startMainApplication.class, args);
+    }
+}
